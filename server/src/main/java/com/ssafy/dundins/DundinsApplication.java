a79155d433feb603a823e1bc815b0d27ff922535
wlwlsus <cadqe13@gmail.com>
2022-11-16 14:50:25 +0900

    first commit

---
+++ server/src/main/java/com/ssafy/dundins/DundinsApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.dundins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DundinsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DundinsApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/com/ssafy/dundins/DundinsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.dundins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DundinsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
