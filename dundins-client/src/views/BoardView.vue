df66cefa3ce14a8ba4c75bd63cf519edde15b252
ejk9658 <ejk9658@naver.com>
2022-11-24 01:09:03 +0900

    Update : header, footer 정리

--- dundins-client/src/views/BoardView.vue
+++ dundins-client/src/views/BoardView.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="boardview">
     <header-navbar></header-navbar>
-    <BasicHeader name="공지사항" />
+    <BasicHeader id="bh-b" name="공지사항" />
     <router-view></router-view>
   </div>
 </template>
@@ -17,7 +17,23 @@ export default {
 </script>
 
 <style>
-/* #boardview {
-  background-color: white;
-} */
+#bh-b {
+  width: 100%;
+  height: 100%;
+  text-align: center;
+  position: relative;
+  z-index: 1;
+}
+#bh-b::after {
+  width: 100%;
+  height: 100%;
+  content: "";
+  background-image: url(https://cdn.pixabay.com/photo/2016/03/27/23/11/post-it-notes-1284667_960_720.jpg);
+  background-size: cover;
+  position: absolute;
+  top: 0;
+  left: 0;
+  z-index: -1;
+  opacity: 0.2;
+}
 </style>
--- dundins-client/src/views/HouseView.vue
+++ dundins-client/src/views/HouseView.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <header-navbar></header-navbar>
-    <BasicHeader name="매물 관리" />
+    <BasicHeader id="bh-h" name="매물 관리" />
     <router-view></router-view>
   </div>
 </template>
@@ -16,4 +16,24 @@ export default {
 };
 </script>
 
-<style></style>
+<style>
+#bh-h {
+  width: 100%;
+  height: 100%;
+  text-align: center;
+  position: relative;
+  z-index: 1;
+}
+#bh-h::after {
+  width: 100%;
+  height: 100%;
+  content: "";
+  background-image: url(https://cdn.pixabay.com/photo/2014/09/18/11/56/skyscrapers-450793_960_720.jpg);
+  background-size: cover;
+  position: absolute;
+  top: 0;
+  left: 0;
+  z-index: -1;
+  opacity: 0.2;
+}
+</style>
